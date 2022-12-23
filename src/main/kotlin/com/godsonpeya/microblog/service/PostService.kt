4d2b1388ac39c5660089751bc5919c2ed12fce05
rkota(Rosaire Kota) <rosairekota@gmail.com>
2022-12-23 19:56:14 +0100

    feat: add postService

---
+++ src/main/kotlin/com/godsonpeya/microblog/service/PostService.kt
@@ -0,0 +1,41 @@
+package com.godsonpeya.microblog.service
+
+import com.godsonpeya.microblog.entity.Post
+import com.godsonpeya.microblog.repository.PostRepository
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.stereotype.Service
+
+
+@Service
+class PostService @Autowired constructor(private var postRepository: PostRepository) {
+
+
+   fun getAll():List<Post> = postRepository.findAll() 
+
+   fun getOne(id: Long ): Post {
+    return postRepository.findById(id).orElseThrow{ IllegalArgumentException("this post is not found")}
+   }
+   fun save(post: Post): Post = postRepository.save(post)
+   fun update(id: Long, postDto: Post): Post {
+      try {
+         val postFound = getOne(id)
+         postFound.content = postDto.content
+         postFound.userId = postDto.userId
+         return postRepository.save(postFound)
+      }
+      catch(Exception e) {
+         throw IllegalArgumentException("error ..")
+      }
+   }
+   fun foo(id:Long): Boolean {
+      postFound = getOne(id)
+       try {
+         postRepository.delete(postFound)
+         return "post deleted"
+       }
+       catch(Exception e) {
+         return "error for "
+       }
+      
+   }
+}
